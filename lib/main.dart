9ff4788dcfe6daf20e5c40d92dd28d2e0c843403
yousef omar <yousifomar698@gmail.com>
2024-02-25 15:08:30 +0200

    stope testing

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,12 @@ import 'package:we_here_together/layout/layout_screen.dart';
 
 void main() {
   runApp(const MyApp());
+
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+
 
   // This widget is the root of your application.
   @override
